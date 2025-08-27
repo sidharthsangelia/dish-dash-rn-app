5dd2607b14e1376e43d3b6ace0ac82c09e263666
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-08-27 20:20:49 +0530

    add: signup and verify code screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,16 @@
+import { COLORS } from "@/constants/colors";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Slot } from "expo-router";
+import { StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaView style={{flex:1}}>
+      <SafeAreaView style={{flex:1, backgroundColor: COLORS.background}}>
         <Slot />
+      
       </SafeAreaView>
     </ClerkProvider>
   );
